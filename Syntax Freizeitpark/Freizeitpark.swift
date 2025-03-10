3343d7d5f3548f0d352415b149be611ad300b7a2
Rylie Castell <rylie.castell@gmail.com>
2025-03-10 15:55:22 +0100

    add amusementpark data

---
+++ Syntax Freizeitpark/Freizeitpark.swift
@@ -0,0 +1,13 @@
+//
+//  Freizeitpark.swift
+//  Syntax Freizeitpark
+//
+//  Created by Rylie Castell on 10.03.25.
+//
+
+import Foundation
+
+let parkName: String = "Syntax Freizeitpark GmbH"
+var isOpen: Bool = false
+let maxGuestNumber: Int = 200
+var guestNumber: Int = 0
