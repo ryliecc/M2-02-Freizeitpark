cb053605f1ebfe154a746b49ae0ecf0837750259
Rylie Castell <rylie.castell@gmail.com>
2025-03-11 13:13:41 +0100

    add vip list

--- Syntax Freizeitpark/main.swift
+++ Syntax Freizeitpark/main.swift
@@ -21,13 +21,13 @@ if (maxGuestNumber == guestNumber) {
 
 // Aufgabe 1.3 Besucherdaten und Attraktionsdaten
 
-let guest1: (name: String, age: Int, heightInM: Double, favoriteFood: String, favoriteAttraction: String) = ("Miley Cyrus", 32, 1.65, "Mashed Potatoes", "Rollercoaster")
+var guest1: (name: String, age: Int, heightInM: Double, favoriteFood: String, favoriteAttraction: String, isVip: Bool) = ("Miley Cyrus", 32, 1.65, "Mashed Potatoes", "Rollercoaster", false)
 
-let guest2: (name: String, age: Int, heightInM: Double, favoriteFood: String, favoriteAttraction: String) = ("Laverne Cox", 15, 1.8, "Beyond Sausage", "Drop Tower")
+let guest2: (name: String, age: Int, heightInM: Double, favoriteFood: String, favoriteAttraction: String, isVip: Bool) = ("Laverne Cox", 15, 1.8, "Beyond Sausage", "Drop Tower", false)
 
-let guest3: (name: String, age: Int, heightInM: Double, favoriteFood: String, favoriteAttraction: String) = ("Tiny Tim", 9, 1.1, "Cotton Candy", "Carousel")
+let guest3: (name: String, age: Int, heightInM: Double, favoriteFood: String, favoriteAttraction: String, isVip: Bool) = ("Tiny Tim", 9, 1.1, "Cotton Candy", "Carousel", false)
 
-let guest4: (name: String, age: Int, heightInM: Double, favoriteFood: String, favoriteAttraction: String) = ("Jada Pinkett Smith", 53, 1.52, "Fruit Bowl", "Boat Ride")
+let guest4: (name: String, age: Int, heightInM: Double, favoriteFood: String, favoriteAttraction: String, isVip: Bool) = ("Jada Pinkett Smith", 53, 1.52, "Fruit Bowl", "Boat Ride", false)
 
 let attraction1: (name: String, capacity: Int, minHeightInM: Double, minAge: Int, location: String, category: String) = ("Donnerfalke", 30, 1.6, 16, "Adventure Land", "Rollercoaster")
 
@@ -38,18 +38,21 @@ let attraction3: (name: String, capacity: Int, minHeightInM: Double, minAge: Int
 
 // Aufgabe 1.4 Einlasskontrolle
 // Aufgabe 1.5 Begründung
+// Aufgabe 1.6 VIP-Liste
 
-func controlParkEntrance(_ guest: (name: String, age: Int, heightInM: Double, favoriteFood: String, favoriteAttraction: String)) {
+func controlParkEntrance(_ guest: (name: String, age: Int, heightInM: Double, favoriteFood: String, favoriteAttraction: String, isVip: Bool)) {
     
     if (!isOpen) {
         print("Der Gast \(guest.name) darf nicht in den Park, weil der Park noch nicht geöffnet ist.")
-    } else if (guest.age >= 12 && maxGuestNumber > guestNumber) {
+    } else if (guest.age >= 12 && maxGuestNumber > guestNumber && !guest.isVip) {
         print("Der Gast \(guest.name) ist \(guest.age) Jahre alt und darf in den Park.")
         guestNumber += 1
-    } else if (guest.age < 12 && maxGuestNumber > guestNumber) {
+    } else if (guest.age < 12 && maxGuestNumber > guestNumber && !guest.isVip) {
         print("Der Gast \(guest.name) ist erst \(guest.age) Jahre alt und darf deswegen nicht in den Park.")
-    } else if (maxGuestNumber <= guestNumber) {
+    } else if (maxGuestNumber <= guestNumber && !guest.isVip) {
         print("Der Gast \(guest.name) darf nicht in den Park, weil der Park bereits voll ist.")
+    } else if (guest.isVip) {
+        print("Der Gast \(guest.name) ist ein VIP und darf in den Park.")
     }
     
 }
@@ -59,7 +62,7 @@ controlParkEntrance(guest3)
 controlParkEntrance(guest2)
 controlParkEntrance(guest4)
 
-func controlDonnerfalkeEntrace(_ guest: (name: String, age: Int, heightInM: Double, favoriteFood: String, favoriteAttraction: String)) {
+func controlDonnerfalkeEntrace(_ guest: (name: String, age: Int, heightInM: Double, favoriteFood: String, favoriteAttraction: String, isVip: Bool)) {
     
     if (guest.age >= 16 && guest.heightInM >= 1.6) {
         print("Der Gast \(guest.name) ist \(guest.age) Jahre alt und \(guest.heightInM.formatted(.number.precision(.fractionLength(2))))m groß und darf auf den Donnerfalken.")
@@ -76,3 +79,14 @@ controlDonnerfalkeEntrace(guest1)
 controlDonnerfalkeEntrace(guest2)
 controlDonnerfalkeEntrace(guest3)
 controlDonnerfalkeEntrace(guest4)
+
+// Aufgabe 1.6 VIP-Liste Daten eingeben
+
+print("Bitte gib deinen Namen ein.")
+guest1.name = readLine()!
+print("Bitte gib dein Alter in Jahren ein.")
+guest1.age = Int(readLine()!)!
+print("Bitte gib true ein, falls du ein VIP bist und false, wenn du keiner bist.")
+guest1.isVip = Bool(readLine()!)!
+print("Es wird nun geprüft, ob du eingelassen wirst.")
+controlParkEntrance(guest1)
